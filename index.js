1ae4d5ceba8bfae125cc44a0fc832cc2952fae15
Pablo Salas <pablosalas@outlook.com.ar>
2021-06-17 18:36:46 +0200

    deploy scripts added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static('build'))
 morgan.token('post', function (req, res,) { return JSON.stringify(req.body)})
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
 

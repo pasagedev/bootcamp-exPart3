924bcfbda2117e98371b96b4ed7a51d3454117f0
Pablo Salas <pablosalas@outlook.com.ar>
2021-06-19 20:28:08 +0200

    ej 3.13-3.14 completed

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+require('dotenv').config()
+const Person = require('./models/person')
 const { request, json } = require('express')
 const express = require('express')
 const morgan = require('morgan')
@@ -8,35 +10,36 @@ const app = express()
 app.use(express.json())
 app.use(cors())
 app.use(express.static('build'))
-morgan.token('post', function (req, res,) { return JSON.stringify(req.body)})
+morgan.token('post', function (req, res,) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
 
 
-let persons = [
-    {
-        id: 1,
-        name: "Arto Hellas",
-        number: "040-123456"
-    },
-    {
-        id: 2,
-        name: "Ada Lovelace",
-        number: "39-44-5323523"
-    },
-    {
-        id: 3,
-        name: "Dan Abramov",
-        number: "12-43-234345"
-    },
-    {
-        id: 4,
-        name: "Marry Poppendick",
-        number: "39-23-6423122"
-    }
-]
+// let persons = [
+//     {
+//         id: 1,
+//         name: "Arto Hellas",
+//         number: "040-123456"
+//     },
+//     {
+//         id: 2,
+//         name: "Ada Lovelace",
+//         number: "39-44-5323523"
+//     },
+//     {
+//         id: 3,
+//         name: "Dan Abramov",
+//         number: "12-43-234345"
+//     },
+//     {
+//         id: 4,
+//         name: "Marry Poppendick",
+//         number: "39-23-6423122"
+//     }
+// ]
 
 app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({})
+        .then(persons => response.json(persons))
 })
 
 app.get('/info', (request, response) => {
@@ -68,23 +71,23 @@ const personExist = person => {
         return false
 }
 app.post('/api/persons', (request, response) => {
-    const person = request.body
+    const body = request.body
     const newID = Math.floor(Math.random() * 1000 + 1)
-    person.id = newID
 
-    if (!person.name)
+    if (!body.name)
         return response.status(400).json({ error: 'name can not be empty' })
-    if (!person.number)
+    if (!body.number)
         return response.status(400).json({ error: 'number can not be empty' })
 
-    if (personExist(person))
-        return response.status(400).json({ error: 'name must be unique' })
-
-    persons = persons.concat(person)
-    response.json(person)
+    const newPerson = new Person({
+        name: body.name,
+        number: body.number
+    })
+    newPerson.save()
+        .then(savedPerson => response.json(savedPerson))
 })
 
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
---
+++ models/person.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to ', url)
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then(() => console.log('Database connected'))
+    .catch(err => {
+        console.log(err)
+        process.exit(1)
+    })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: Number
+})
+
+personSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model('Person', personSchema)

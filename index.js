7eec9df277e6f98fff2906c1cf0b1af92a373379
Pablo Salas <pablosalas@outlook.com.ar>
2021-06-23 18:48:50 +0200

    uibuild

--- index.js
+++ index.js
@@ -13,30 +13,6 @@ app.use(express.static('build'))
 morgan.token('post', function (req, res,) { return JSON.stringify(req.body) })
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :post'))
 
-
-// let persons = [
-//     {
-//         id: 1,
-//         name: "Arto Hellas",
-//         number: "040-123456"
-//     },
-//     {
-//         id: 2,
-//         name: "Ada Lovelace",
-//         number: "39-44-5323523"
-//     },
-//     {
-//         id: 3,
-//         name: "Dan Abramov",
-//         number: "12-43-234345"
-//     },
-//     {
-//         id: 4,
-//         name: "Marry Poppendick",
-//         number: "39-23-6423122"
-//     }
-// ]
-
 app.get('/api/persons', (request, response) => {
     Person.find({})
         .then(persons => response.json(persons))
